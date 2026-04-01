0fff6801ba8ac7f5c737ad84e2864bef05c0784a
aronboliveira <aron.b.96@gmail.com>
2026-04-01 04:49:49 -0300

    fix(routes): update route definitions

--- _inc/laravel/routes/web.php
+++ _inc/laravel/routes/web.php
@@ -223,9 +223,6 @@ R::group(['middleware' => [MWC::VF]], function () {
     R::get('/account-dashboard', [DSBC::class, DSBC::ACC_DSB_IDX])
         ->name(DSBC::ENTITY)
         ->middleware([MWC::AUTH, MWC::XSS, MWC::REV]);
-    R::get('/account-dashboard/chart-data', [DSBC::class, DSBC::CHART_DATA])
-        ->name('dashboard.chart-data')
-        ->middleware([MWC::AUTH, MWC::XSS]);
     R::get('/project-dashboard', [DSBC::class, DSBC::PRJ_DSB_IDX])
         ->name(VW::PRJ . '.dashboard')
         ->middleware([MWC::AUTH, MWC::XSS, MWC::REV]);
@@ -330,7 +327,7 @@ R::group(['middleware' => [MWC::VF]], function () {
             R::post('cookie-setting', [SystemController::class, SystemController::SV_CK_ST])->name(VW::SET . '.cookies.store');
             R::post('cache-settings', [SystemController::class, SystemController::CC_ST_STR])->name('cache.settings.store')
                 ->middleware([MWC::AUTH, MWC::XSS]);
-            R::resource('systems', SystemController::class)->only(['index', 'store']);
+            R::resource('systems', SystemController::class);
         }
     );
     #endregion
@@ -365,7 +362,7 @@ R::group(['middleware' => [MWC::VF]], function () {
         function () {
             R::get(VW::CST . '/{id}/show', [CustomerController::class, 'show'])
                 ->name(VW::CST . '.show');
-            R::resource(VW::CST, CustomerController::class)->where(['customer' => '[0-9]+']);
+            R::resource(VW::CST, CustomerController::class);
         }
     );
     #endregion
@@ -383,7 +380,7 @@ R::group(['middleware' => [MWC::VF]], function () {
         function () {
             R::get(VW::VND . '/{id}/show', [VendorController::class, 'show'])
                 ->name(VW::VND . '.show');
-            R::resource(VW::VND, VendorController::class)->where(['vendor' => '[0-9]+']);
+            R::resource(VW::VND, VendorController::class);
         }
     );
     #endregion
@@ -463,7 +460,7 @@ R::group(['middleware' => [MWC::VF]], function () {
                 ->name(VW::INV . '.payment.destroy');
             R::get(VW::INV . '/items', [InvoiceController::class, 'items'])->name(VW::INV . '.items');
             R::get(VW::INV . '/create/{cid}', [InvoiceController::class, 'create'])->name(VW::INV . '.create');
-            R::resource(VW::INV, InvoiceController::class)->where(['invoice' => '[0-9]+']);
+            R::resource(VW::INV, InvoiceController::class);
         }
     );
     R::get(VW::INV . '/preview/{template}/{color}', [InvoiceController::class, InvoiceController::INV_PRV])->name(VW::INV . '.preview');
@@ -566,7 +563,7 @@ R::group(['middleware' => [MWC::VF]], function () {
             R::post(VW::BIL . '{id}/payment/{pid}/destroy', [BillController::class, BillController::PAY_DST])->name(VW::BIL . '.payment.destroy');
             R::get(VW::BIL . 'items', [BillController::class, 'items'])->name(VW::BIL . '.items');
             R::get(VW::BIL . 'create/{cid}', [BillController::class, 'create'])->name(VW::BIL . '.create');
-            R::resource(VW::BIL, BillController::class)->where(['bill' => '[0-9]+']);
+            R::resource(VW::BIL, BillController::class);
         }
     );
     #endregion
@@ -669,7 +666,7 @@ R::group(['middleware' => [MWC::VF]], function () {
             R::get(VW::PPS . '/{id}/sent', [ProposalController::class, 'sent'])->name(VW::PPS . '.sent');
             R::get(VW::PPS . '/{id}/resent', [ProposalController::class, 'resent'])->name(VW::PPS . '.resent');
             R::get(VW::PPS . '/create/{cid}', [ProposalController::class, 'create'])->name(VW::PPS . '.create');
-            R::resource('proposal', ProposalController::class)->where(['proposal' => '[0-9]+']);
+            R::resource('proposal', ProposalController::class);
         }
     );
     R::get(VW::PPS . '/preview/{template}/{color}', [ProposalController::class, ProposalController::PV_PPS])->name(VW::PPS . '.preview');
@@ -858,10 +855,6 @@ R::group(['middleware' => [MWC::VF]], function () {
 
     R::get('last-login', [EmployeeController::class, EmployeeController::LST_LGN])->name('last_login')->middleware([MWC::AUTH, MWC::XSS]);
 
-    R::get(VW::EMP . '/export', [EmployeeController::class, 'export'])->name(VW::EMP . '.export')->middleware([MWC::AUTH, MWC::XSS]);
-    R::get(VW::EMP . '/import/file', [EmployeeController::class, EmployeeController::IMP_FL])->name(VW::EMP . '.file.import')->middleware([MWC::AUTH, MWC::XSS]);
-    R::post(VW::EMP . '/import/index', [EmployeeController::class, 'import'])->name(VW::EMP . '.import')->middleware([MWC::AUTH, MWC::XSS]);
-
     R::resource(VW::EMP, EmployeeController::class)->middleware([MWC::AUTH, MWC::XSS]);
 
     R::post(VW::EMP . '/getdepartment', [EmployeeController::class, EmployeeController::GET_DPT])->name(VW::EMP . '.getdepartment')->middleware([MWC::AUTH, MWC::XSS]);
@@ -887,16 +880,16 @@ R::group(['middleware' => [MWC::VF]], function () {
     R::get(VW::OT_PAY . '/create/{eid}', [OtherPaymentController::class, OtherPaymentController::OT_PAY_CR])->name(VW::OT_PAY . '.create')->middleware([MWC::AUTH, MWC::XSS]);
     R::get(VW::OVT . '/create/{eid}', [OvertimeController::class, 'overtimeCreate'])->name(VW::OVT . '.create')->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::S_SLR, SetSalaryController::class)->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource(VW::ALW, AllowanceController::class)->except(['create'])->middleware([MWC::AUTH, MWC::XSS]);
+    R::resource(VW::ALW, AllowanceController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::ALW_OPT, AllowanceOptionController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::COM, CommissionController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::DDT_OPT, DeductionOptionController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::LN_OPT, LoanOptionController::class)->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource(VW::LN, LoanController::class)->except(['create'])->middleware([MWC::AUTH, MWC::XSS]);
+    R::resource(VW::LN, LoanController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::PY_SLP, PayslipTypeController::class)->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource(VW::STR_DD, SaturationDeductionController::class)->except(['index', 'create'])->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource(VW::OT_PAY, OtherPaymentController::class)->except(['create'])->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource(VW::OVT, OvertimeController::class)->except(['create'])->middleware([MWC::AUTH, MWC::XSS]);
+    R::resource(VW::STR_DD, SaturationDeductionController::class)->middleware([MWC::AUTH, MWC::XSS]);
+    R::resource(VW::OT_PAY, OtherPaymentController::class)->middleware([MWC::AUTH, MWC::XSS]);
+    R::resource(VW::OVT, OvertimeController::class)->middleware([MWC::AUTH, MWC::XSS]);
     //================================= Payslips Controller ====================================//
     #region
     R::get(VW::PY_SLP . '/paysalary/{id}/{date}', [PayslipController::class, PayslipController::PAY_SLR])->name(VW::PY_SLP . '.paysalary')->middleware([MWC::AUTH, MWC::XSS]);
@@ -911,7 +904,7 @@ R::group(['middleware' => [MWC::VF]], function () {
     R::get(VW::PY_SLP . '/payslipPdf/{id}', [PayslipController::class, PayslipController::PAY_SLP_PDF])->name(VW::PY_SLP . '.payslipPdf')->middleware([MWC::AUTH, MWC::XSS]);
     R::get(VW::PY_SLP . '/send/{id}/{m}', [PayslipController::class, 'send'])->name(VW::PY_SLP . '.send')->middleware([MWC::AUTH, MWC::XSS]);
     R::get(VW::PY_SLP . '/delete/{id}', [PayslipController::class, 'destroy'])->name(VW::PY_SLP . '.delete')->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource(VW::PY_SLP, PayslipController::class)->except(['create'])->middleware([MWC::AUTH, MWC::XSS]);
+    R::resource(VW::PY_SLP, PayslipController::class)->middleware([MWC::AUTH, MWC::XSS]);
     #endregion
     R::post(VW::BRC . '/' . VW::EMP . '/json', [EmployeeController::class, EmployeeController::EMP_JSON])->name(VW::BRC . '.employee.json')->middleware([MWC::AUTH, MWC::XSS]);
     R::post(VW::EVT . '/get-department', [EventController::class, EventController::GET_DPT])->name(VW::EVT . '.getdepartment')->middleware([MWC::AUTH, MWC::XSS]);
@@ -999,7 +992,7 @@ R::group(['middleware' => [MWC::VF]], function () {
     R::resource(VW::TRF, TransferController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::EMP_ATD . '', EmployeeAttendanceController::class)->middleware([MWC::AUTH, MWC::XSS]);
     R::resource(VW::LV_TP, LeaveTypeController::class)->middleware([MWC::AUTH, MWC::XSS]);
-    R::resource('leave', LeaveController::class)->middleware([MWC::AUTH, MWC::XSS])->where(['leave' => '[0-9]+']);
+    R::resource('leave', LeaveController::class)->middleware([MWC::AUTH, MWC::XSS]);
     //crm report
     R::get(VW::RPT . '-lead', [RPC::class, 'leadReport'])->name(VW::RPT . '.lead')->middleware([MWC::AUTH, MWC::XSS]);
     R::get(VW::RPT . '-deal', [RPC::class, 'dealReport'])->name(VW::RPT . '.deal')->middleware([MWC::AUTH, MWC::XSS]);
@@ -1124,7 +1117,7 @@ R::group(['middleware' => [MWC::VF]], function () {
     R::post(VW::PRJ . '/{id}/' . VW::EXP . '/{eid}', [ExpenseController::class, 'update'])->name(VW::PRJ_EXP . '.update')->middleware([MWC::AUTH, MWC::XSS]);
     R::delete(VW::PRJ . '/{eid}/' . VW::EXP . '/', [ExpenseController::class, 'destroy'])->name(VW::PRJ_EXP . '.destroy')->middleware([MWC::AUTH, MWC::XSS]);
     // TODO missing method
-    R::get('/expense-list', [ExpenseController::class, 'index'])->name(VW::EXP . '.list')->middleware([MWC::AUTH, MWC::XSS]);
+    R::get('/expense-list', [ExpenseController::class, 'expenseList'])->name(VW::EXP . '.list')->middleware([MWC::AUTH, MWC::XSS]);
     #endregion
 
     //================================= Contract Types ====================================//
@@ -1388,6 +1381,10 @@ R::group(['middleware' => [MWC::VF]], function () {
     R::get(VW::PPS . '/export', [ProposalController::class, 'export'])->name(VW::PPS . '.export');
     R::get(VW::BIL . '/export', [BillController::class, 'export'])->name(VW::BIL . '.export');
 
+    R::get(VW::EMP . '/export', [EmployeeController::class, 'export'])->name(VW::EMP . '.export');
+    R::get(VW::EMP . '/import/file', [EmployeeController::class, EmployeeController::IMP_FL])->name(VW::EMP . '.file.import');
+    R::post(VW::EMP . '/import/index', [EmployeeController::class, 'import'])->name(VW::EMP . '.import');
+
     R::get('attendance/import/file', [EmployeeAttendanceController::class, 'importFile'])->name('attendance.file.import');
     R::post('attendance/import/index', [EmployeeAttendanceController::class, 'import'])->name('attendance.import');
 
@@ -1396,7 +1393,7 @@ R::group(['middleware' => [MWC::VF]], function () {
     //================================= Product Stock ====================================//
     #region
     R::group(['middleware' => [MWC::SET]], function () {
-        R::get(VW::PRD_STK . '/export', [RPC::class, 'stockExport'])->name(VW::PRD_STK . '.export');
+        R::get(VW::PRD_STK . '/export', [RPC::class, 'stock_export'])->name(VW::PRD_STK . '.export');
         R::resource(VW::PRD_STK, ProductStockController::class)->middleware([MWC::AUTH, MWC::XSS, 'check.mount']);
     });
     #endregion
