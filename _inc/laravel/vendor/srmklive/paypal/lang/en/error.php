9d485e0607b4ed5bb4d2ca4b5f8758900e3c0e74
aronboliveira <aron.b.96@gmail.com>
2026-05-02 14:51:52 -0300

    chore(gitignore): normalize ignore cascade

--- _inc/laravel/vendor/srmklive/paypal/lang/en/error.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'paypal_transaction_declined'     => 'Unable to process payment because your transaction was declined on PayPal.',
-    'paypal_transaction_not_verified' => 'Unable to verify the PayPal transaction.',
-    'paypal_connection_error'         => 'Unable to connect to PayPal. Please try again.',
-];
